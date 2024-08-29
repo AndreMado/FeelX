21f3b2ed2f4b3b7372fd0f135c755b176662fdb7
Andres Cortazar <andresxmaldonado@gmail.com>
2024-08-28 20:37:34 -0600

    Idk if that work

--- dags/sentiment_analysis_dag.py
+++ dags/sentiment_analysis_dag.py
@@ -1,54 +1,34 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2021, 1, 1),
+    'start_date': datetime(2023, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
     'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG(
-    'full_workflow_orchestration',
+    'data_pipeline_control',
     default_args=default_args,
-    description='Orchestrate Zookeeper, Kafka, Producer, and Spark',
-    schedule_interval=None,  # Se ejecuta bajo demanda
+    description='Controla el flujo de datos ejecutando producer y consumer',
+    schedule_interval='@hourly',
 )
 
-# 1. Iniciar Zookeeper
-start_zookeeper = BashOperator(
-    task_id='start_zookeeper',
-    bash_command='docker start diciembre27-zookeeper-1',  # O 'docker-compose up -d zookeeper'
+run_producer = BashOperator(
+    task_id='run_producer',
+    bash_command='docker exec -d diciembre27-producer-1 python /app/scripts/producer.py',
     dag=dag,
 )
 
-# 2. Iniciar Kafka (después de Zookeeper)
-start_kafka_1 = BashOperator(
-    task_id='start_kafka_1',
-    bash_command='docker start diciembre27-kafka-1-1',  # O 'docker-compose up -d kafka-1'
+run_consumer = BashOperator(
+    task_id='run_consumer',
+    bash_command='docker exec -d diciembre27-spark-1 spark-submit /opt/bitnami/spark/scripts/consumer.py',
     dag=dag,
 )
 
-start_kafka_2 = BashOperator(
-    task_id='start_kafka_2',
-    bash_command='docker start diciembre27-kafka-2-1',  # O 'docker-compose up -d kafka-2'
-    dag=dag,
-)
-
-# 3. Iniciar el Producer (después de Kafka)
-start_producer = BashOperator(
-    task_id='start_producer',
-    bash_command='docker start diciembre27-producer-1',  # O 'docker-compose up -d producer'
-    dag=dag,
-)
-
-# 4. Iniciar Spark (después de que el Producer esté activo)
-start_spark = BashOperator(
-    task_id='start_spark',
-    bash_command='docker start diciembre27-spark-1',  # O 'docker-compose up -d spark'
-    dag=dag,
-)
-
-# Definir las dependencias en el DAG
-start_zookeeper >> [start_kafka_1, start_kafka_2] >> start_producer >> start_spark
+run_producer >> run_consumer
